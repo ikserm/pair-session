18d767005cf4beda1debb2e17c0c76fb9fb048cb
ikserm <iksermd@gmail.com>
2021-02-12 05:56:15 +0000

    ++

--- test/app.ts
+++ test/app.ts
@@ -1,3 +1,4 @@
+/** Testing code **/
 import { App } from "../src/app";
 
 import { expect } from "chai"; 
@@ -9,6 +10,6 @@ describe('Value absolute sum function', () => {
     })   
     it('Should return +3 when introduce -2,-1', () => {
         let app = new App()
-        expect(app.sumAbsoluteValue(-1,-2)).equal(5)
+        expect(app.sumAbsoluteValue(-1,-2)).equal(3)
     })
 });
